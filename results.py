bccf2a64e3a0414a207c27fb59e4e86bd1f058ca
alirezamhm <alireza.am1379@gmail.com>
2024-08-01 18:22:04 +0300

    add load model

--- results.py
+++ results.py
@@ -4,6 +4,8 @@ import pandas as pd
 import wandb
 
 from src.dataset import corruptions
+from src.posterior_network import PosteriorNetwork
+from src.baseline import Baseline
 
 def extract_wandb():
     runs = wandb.Api().runs('alirezamhm/posterior-network-under-diffusion')
@@ -28,6 +30,15 @@ def print_summary(metrics, names):
         corr_error = np.mean([metrics[i][f'error/{c}_{k+1}'] for c in corruptions for k in range(5)])
 
         print(f'{i+1:03d} | loss {loss:.4f} error {error:.4f}  corr_loss {corr_loss:.4f} corr_error {corr_error:.4f} | {id} ')        
+
+def load_model(args, fn, res, num_classes, class_counts, device=torch.device('cpu')):
+    if args.type == 'baseline':
+        model = Baseline(args, res, num_classes)
+    elif args.type == 'posterior-network':
+        model = PosteriorNetwork(args, res, num_classes, class_counts)
+    checkpoint = torch.load(fn, map_location=device)
+    model.load_state_dict(checkpoint['state_dict'])
+    return model
 
 if __name__=='__main__':
     metrics, configs, names = extract_wandb()
